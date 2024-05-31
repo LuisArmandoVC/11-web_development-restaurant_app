7ecb7fc80228e70541bdc3ac31811995f0ddaea2
Luis Armando Veliz Cruz <luisarmandoveliz826@gmail.com>
2024-05-30 22:06:49 -0500

    [checkout page] tip front + total price adjust logic completed

--- web_dev_restaurant_app/core/static/core/js/header.js
+++ web_dev_restaurant_app/core/static/core/js/header.js
@@ -39,22 +39,28 @@ const headerShoppingCar = () => {
 }
 document.addEventListener("DOMContentLoaded", headerShoppingCar());
 
-// 3. Popup location logic
-// 3.1. Custom height for popup__background elemento depending on body height 
+
+
+// 3. Popup location logic. You will see same logic order as user completes popup location flow
+
+
+
+// 3.1. height for popup__background
+// Description: popup__background is a element located on master.html template. It is a hidden black background that appears to contrast popup form with body
+//              this function will change its height variably, depending on current body height. TIP: body height might change in home, about, catalog or other landing page
 window.onload = function() {
     var fullHeightPopupBackground = document.querySelector('.popup__background');
     var bodyHeight = document.body.scrollHeight;
     fullHeightPopupBackground.style.height = bodyHeight + 'px';
 }
-// 3.2. Autocomplete user address using google services. 
+// 3.2. Key definitions
+// Description: this objects defines a geopositioning polygon which means delivery area on initial project stage, including its bounds. Center means polygon center 
 let autocomplete;
 const center = { lat: 4.651657, lng: -74.130889 };
-
 const north = center.lat + 0.045;
 const south = center.lat - 0.018; 
 const west = center.lng - 0.010;  
 const east = center.lng + 0.045; 
-
 const polygon = [
     { lat: north, lng: center.lng },
     { lat: center.lat, lng: east },
@@ -62,13 +68,14 @@ const polygon = [
     { lat: center.lat, lng: west },
     { lat: north, lng: center.lng },
 ];
-
 const defaultBounds = {
     north: center.lat + 0.2,
     south: center.lat - 0.2,
     east: center.lng + 0.2,
     west: center.lng - 0.2,
 };
+// 3.3 auto complete function
+// Description: google function that reads popup input (screen #1 - id=autocomplete). It call places services to autocomplete user address based on initial input
 function initAutocomplete(){
     autocomplete = new google.maps.places.Autocomplete(
         document.getElementById('autocomplete'),
@@ -81,7 +88,10 @@ function initAutocomplete(){
     );
     autocomplete.addListener('place_changed', onPlaceChanged);
 }
-// 3.3 Retrieve key information by user.
+// 3.3.1  Definitions & Auxiliar function
+// Description: key variables to be mapped in google in order to be confirmed by user (placeLat and placeLng). placeName is helpful to map in frontend so business and user will
+//              identify their locations. Place name could be found in header and checkout section
+//              auxiliar function "onPlaceChanged" its a google function that triggers once user click on auto completed option. It gave us lat, lng and place name
 let placeName;
 let placeLat;
 let placeLng;
@@ -89,7 +99,6 @@ let inside = false;
 
 function onPlaceChanged(){
     place = autocomplete.getPlace();
-
     if (!place.geometry) {
         console.log('enter a place!');
     }
@@ -98,14 +107,10 @@ function onPlaceChanged(){
         placeName = place.name;
         placeLat = place.geometry.location.lat();
         placeLng = place.geometry.location.lng();
-
     }
 }
-
-// 3.5 CTA user clicks after introduce location info
-// description: This function will hide stage 1 and show stage 2 of location popup
-
-
+// 3.4 CTA user clicks after introduce location info
+// description: This function will hide stage 1 and show stage 2 of location popup.
 function lookForLocation(btnArg) {
     const stage__1 = document.querySelector('#stage__1');
     const stage__2 = document.querySelector('#stage__2');
@@ -122,9 +127,7 @@ function lookForLocation(btnArg) {
     {
         currentPosition();
     }
-
     const confirmAddressBtn = document.querySelector('.confirmAddressBtn');
-
     setTimeout(()=> {
         if(!inside)
         {
@@ -134,17 +137,12 @@ function lookForLocation(btnArg) {
         else if (inside && confirmAddressBtn.classList.contains('btn-not-allowed')) {
             confirmAddressBtn.classList.remove('btn-not-allowed');
             confirmAddressBtn.disabled = false;
-
         }
     }, 350)
 }
-
-
-
-
-// 3.4 Print map with coords given by user, add a marker also
+// 3.5 Print map with coords given by user, add a marker also
+// Description: this function will execute only if user decides to use autocomplete options (it triggers if user clicks in magnifying glass icon)
 function initMap() {
-
     centerUser = { lat: placeLat, lng: placeLng };
     location__name = document.querySelector('#location__name');
     location__name.innerHTML = placeName.toString();
@@ -165,18 +163,18 @@ function initMap() {
         inside = false;
     }
 }
-// 3.6 get current position if user decides it 
+// 3.6 get current position & print them in google maps
+// Description: this function triggers if user clicks on "Usar mi ubicacion" which means did not use autocomplete option but we read user location through browser. 
+//              then, we use geocode google services to translate coords into map location
 function currentPosition() {
     navigator.geolocation.getCurrentPosition(showPosition,showError);
 }
-
 async function showPosition(position) {
     placeLat = position.coords.latitude;
     placeLng = position.coords.longitude;
     let latlng = `${placeLat},${placeLng}`
     var url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + latlng + "&key=AIzaSyAm7duhLPL5kqqtkOHH1qWEmaX0oU9a2NQ";
     let centerUser = { lat: placeLat, lng: placeLng };
-
     try {
         const response = await fetch(url);
         if (!response.ok) {
@@ -188,7 +186,6 @@ async function showPosition(position) {
     catch (error) {
         console.error('Error durante el proceso:', error);
     }
-
     location__name = document.querySelector('#location__name');
     location__name.innerHTML = placeName.toString();
     let map = new google.maps.Map(document.getElementById('map'), {
@@ -200,7 +197,6 @@ async function showPosition(position) {
         map: map,
         title: 'Mi Ubicación'
     });
-    
     if (pointInPolygon(centerUser, polygon)) {
         inside = true;
     }
@@ -209,7 +205,6 @@ async function showPosition(position) {
         inside = false;
     }
 }
-
 function showError(error) {
     switch (error.code) {
         case error.PERMISSION_DENIED:
@@ -226,8 +221,8 @@ function showError(error) {
         break;
     }
 }
-
 // 3.7 ray-casting algorithm
+// Description: this algorithm reads delivery zone and user location. If user is in geo polygon then, we enable continue button. If not, then continue button will be disabled
 function pointInPolygon(point, polygon) {
     const x = point.lat, y = point.lng;
     let inside = false;
@@ -239,12 +234,9 @@ function pointInPolygon(point, polygon) {
     }
     return inside;
 }
-
-// 3.6 Go back if user is in stage 2 and wants to modify something else
+// 3.8 Go back if user is in stage 2 and wants to modify something else
 function goBack() {
-
     document.querySelector('#autocomplete').value = "";
-
     const stage__1 = document.querySelector('#stage__1');
     const stage__2 = document.querySelector('#stage__2');
     if (!stage__2.classList.contains('hidden') && stage__1.classList.contains('hidden')) {
@@ -252,28 +244,24 @@ function goBack() {
         stage__1.classList.remove('hidden')
     }
 }
-
-// 3.8 Confirm address
+// 3.9 Confirm address
+// description: This function will hide stage 2 and show stage 3 of location popup.
 function saveAddress() {
     if (inside) {
         const stage__2 = document.querySelector('#stage__2');
         const stage__3 = document.querySelector('#stage__3');
         const location__name_stage3 = document.querySelector('#location__name_stage3');
-    
         location__name_stage3.innerHTML = placeName.toString();
-    
         if (!stage__2.classList.contains('hidden') && stage__3.classList.contains('hidden')) {
             stage__2.classList.add('hidden')
             stage__3.classList.remove('hidden')
         }
     }
 }
-
+// 3.10 Go back from stage 3 to stage 1
+// description: This function will hide stage 3 and show stage 1 of location popup.
 function goBackSt3() {
-
     document.querySelector('#autocomplete').value = "";
-
-
     const stage__1 = document.querySelector('#stage__1');
     const stage__3 = document.querySelector('#stage__3');
     if (!stage__3.classList.contains('hidden') && stage__1.classList.contains('hidden')) {
@@ -281,12 +269,10 @@ function goBackSt3() {
         stage__1.classList.remove('hidden')
     }
 }
-
-// 3.10 Add user location to local storage
+// 3.11 Add user location to local storage
+// Description: once everything is set up, we save user location information in local storage and close popup
 function completeAddressProcess(){
-
     details = document.querySelector('#addressDescription').value;
-
     userLocation = {
         'name': placeName,
         'addressDetails': details,
@@ -294,38 +280,31 @@ function completeAddressProcess(){
         'lng': placeLng,
         'inside': inside
     }
-
     localStorage.setItem("userLocation", JSON.stringify(userLocation));
     checkUserLocation();
-
     closePopup();
 }
+// 3.12 Close and open popups function. These functions will handle all DOM logic related to open and close location popup
 function closePopup() {
     popup = document.querySelector('.dialog');
     popup__background = document.querySelector('.popup__background');
-
     if (!popup__background.classList.contains('hidden') && !popup.classList.contains('popupClosed')) {
         popup.classList.add('popupClosed');
         popup__background.classList.add('hidden');
-
         popup__background.style.height = 0 + 'px';
     }
 }
 function openPopup() {
     popup = document.querySelector('.dialog');
     popup__background = document.querySelector('.popup__background');
-
     if (popup__background.classList.contains('hidden') && popup.classList.contains('popupClosed')) {
         popup.classList.remove('popupClosed');
         popup__background.classList.remove('hidden');
-
         var bodyHeight = document.body.scrollHeight;
         popup__background.style.height = bodyHeight + 'px';
-
     }
 }
-
-// document.addEventListener("itemInserted", checkUserLocation, false);
+// 3.13 paint user location in all frontend spaces that uses ".headerLocation" class
 document.addEventListener('DOMContentLoaded', checkUserLocation());
 
 function checkUserLocation() {
--- web_dev_restaurant_app/products/static/products/js/checkout.js
+++ web_dev_restaurant_app/products/static/products/js/checkout.js
@@ -35,11 +35,44 @@ function calculateTotal(platesArray) {
     return total
 }
 // 5. Once everything is calculated this function will proceed to map everything up
-function mappingDOM(total, serviceFee, shippingCost) {
+function mappingDOM(total, serviceFee, shippingCost, tip, discount) {
+    let tipDefault = 0;
+    let discountDefault = 0;
     document.querySelector('.productCosts').innerHTML = `$${total}`;
     document.querySelector('.serviceFeeCosts').innerHTML = `$${serviceFee}` ;
     document.querySelector('.shippingCost').innerHTML =  `$${shippingCost}`;
-    document.querySelector('.orderTotal').innerHTML =  `$${total + serviceFee + shippingCost}`;
+    if (tip != undefined) {
+        tipDefault = tip
+        //Execution block for summary component
+        let summaryTip = document.querySelector('.summaryTip');
+        let summaryTipBlock = document.querySelector('.summaryTipBlock');
+        summaryTip.innerHTML=`$${tip}`
+        if (summaryTipBlock.classList.contains('hidden')) {
+            summaryTipBlock.classList.remove('hidden');
+            summaryTipBlock.classList.add('flex');
+        }
+        //Execution block for detail view popup component
+        let tipDetail = document.querySelector('.tipDetail');
+        let tipDetailBlock = document.querySelector('.tipDetailBlock');
+        tipDetail.innerHTML=`${tip}`
+        if (tipDetailBlock.classList.contains('hidden')) {
+            tipDetailBlock.classList.remove('hidden');
+            tipDetailBlock.classList.add('flex');
+        }
+    }
+    if (discount != undefined) {
+        discountDefault = discount
+        let summaryTip = document.querySelector('.summaryTip');
+        let summaryTipBlock = document.querySelector('.summaryTipBlock');
+        summaryTip.innerHTML=`$${tip}`
+        if (summaryTipBlock.classList.contains('hidden')) {
+            summaryTipBlock.classList.remove('hidden');
+            summaryTipBlock.classList.add('flex');
+        }
+    }
+    document.querySelector('.orderTotal').innerHTML =  `$${total + serviceFee + shippingCost + tipDefault + discountDefault}`;
+    document.querySelector('.totalDetail').innerHTML =  `${total + serviceFee + shippingCost + tipDefault + discountDefault}`;
+    document.querySelector('.preTotalDetail').innerHTML =  `${total + serviceFee + shippingCost + tipDefault + discountDefault}`;
 }
 // 6. Apply submit button logic based on payment method selected by user
 function listenPaymentMethod() {
@@ -79,7 +112,7 @@ const checkoutFunction = () => {
     decryptData();
     validationProducts(products.length)
     total = calculateTotal(products);
-    mappingDOM(total, 2000, 2000)
+    mappingDOM(total, 2000, 2000, undefined, undefined);
     listenPaymentMethod();
 }
 document.addEventListener("DOMContentLoaded", checkoutFunction());
@@ -130,9 +163,34 @@ const clickingPayment = () => {
 }
 
 
+// 8. List all saved products in detail popup
+// 8.1. Read DOM detail to insert products
+document.addEventListener("DOMContentLoaded", () => {
+    let subtotalProducts = document.querySelector('#subtotalProducts');
+    products.forEach(product => {
+        let productName = product.name;
+        let productPrice = product.individual_price;
+        subtotalProducts.innerHTML += `
+            <div class="mt-5 flex items-start justify-between">
+                <p>${ productName }</p>
+                <div class="flex items-center">
+                    <span>$</span><p>${ productPrice }</p>
+                </div>
+            </div>
+        `
+    });
+    subtotalProducts.innerHTML += `
+        <div class="mt-5 flex items-start justify-between font-bold">
+            <p>Subtotal de productos</p>
+            <div class="flex items-center">
+                <span>$</span><p>${calculateTotal(products)}</p>
+            </div>
+        </div>
+    `
+});
 
 // 9. Click functions 
-    // 9.1 Display tooltip if user want to change address in checkout page
+// 9.1 Display tooltip if user want to change address in checkout page
 const addressTooltip = () => {
     let infoSvg = document.querySelector('.infoAddress');
     if(infoSvg.classList.contains('hidden'))
@@ -145,5 +203,91 @@ const addressTooltip = () => {
         infoSvg.classList.add('hidden');
         infoSvg.classList.remove('flex');
     }
+}
+// 9.2 Tip function
+const tipping = (percentage, domObj) => {
+    let tipBtns = document.querySelectorAll(".tipBtn");
+    let totalPrice = calculateTotal(products) + 4000;
+    let tip = 0;
+    tipBtns.forEach(tipBtn => {
+        if (tipBtn.classList.contains('tipBtn_active')) {
+            tipBtn.classList.remove('tipBtn_active')
+        }
+    });
+    domObj.classList.add('tipBtn_active');
+    if(percentage == '5')
+    {
+        tip = totalPrice*0.05
+        mappingDOM(totalPrice, 2000, 2000, tip)
+    }
+    else if(percentage == '8')
+    {
+        tip = totalPrice*0.08
+        mappingDOM(totalPrice, 2000, 2000, tip)
+    }
+    else if(percentage == '10')
+    {
+        tip = totalPrice*0.1
+        mappingDOM(totalPrice, 2000, 2000, tip)
+    }
+    else
+    {
+        openCheckoutPopup("tippingDialog");
+        let noTip = document.querySelector(".noTip");
+        let saveCustomTip = document.querySelector(".saveCustomTip");
+        noTip.addEventListener('click', () => {
+            tip = totalPrice*0
+            mappingDOM(totalPrice, 2000, 2000, tip);
+            closeCheckoutPopup("tippingDialog");
+        });
+        saveCustomTip.addEventListener('click', () => {
+            let tipCustomValue = document.querySelector(".tipCustomValue").value;
+            tip = parseInt(tipCustomValue);
+            mappingDOM(totalPrice, 2000, 2000, tip);
+            closeCheckoutPopup("tippingDialog");
+        })
+    }
+}
 
+// 9.3 Popup logic handling
+const openCheckoutPopup = (popupType) => {
+    let popup;
+    popup__background = document.querySelector('.popup__background');
+    if (popupType == 'ourCosts') {
+        popup = document.querySelector('.dialogOurCost');
+    }
+    else if(popupType == 'detail')
+    {
+        popup = document.querySelector('.dialogDetail');
+    }
+    else if(popupType == 'tippingDialog')
+    {
+        popup = document.querySelector('.tippingDialog');
+    }
+    if (popup__background.classList.contains('hidden') && popup.classList.contains('popupClosed')) {
+        popup.classList.remove('popupClosed');
+        popup__background.classList.remove('hidden');
+        var bodyHeight = document.body.scrollHeight;
+        popup__background.style.height = bodyHeight + 'px';
+    }
+}
+const closeCheckoutPopup = (popupType) => {
+    let popup
+    popup__background = document.querySelector('.popup__background');
+    if (popupType == 'ourCosts') {
+        popup = document.querySelector('.dialogOurCost');
+    }
+    else if(popupType == 'detail')
+    {
+        popup = document.querySelector('.dialogDetail');
+    }
+    else if(popupType == 'tippingDialog')
+    {
+        popup = document.querySelector('.tippingDialog');
+    }
+    if (!popup__background.classList.contains('hidden') && !popup.classList.contains('popupClosed')) {
+        popup.classList.add('popupClosed');
+        popup__background.classList.add('hidden');
+        popup__background.style.height = 0 + 'px';
+    }
 }
